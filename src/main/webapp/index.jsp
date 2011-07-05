63310e4b55da5403b82044cf34fc9bc0a3f92075
Aase Mestad <aase.mestad@gmail.com>
2011-07-05 22:54:16 +0200

    keyboard control virker.

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,7 +6,8 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <script language="javascript" src="js/drawing.js"></script>
+        <script type="text/javascript" src="js/drawing.js"></script>
+        <script type="text/javascript" src="js/control.js"></script>
         <title>JSP Page</title>
     </head>
     <body onload="init()">
--- src/main/webapp/js/control.js
+++ src/main/webapp/js/control.js
@@ -1,15 +1,32 @@
 var rightDown = false;
 var leftDown = false;
+var downDown = false;
+var upDown = false;
 
 //set rightDown or leftDown if the right or left keys are down
 function onKeyDown(evt) {
-  if (evt.keyCode == 39) rightDown = true;
-  else if (evt.keyCode == 37) leftDown = true;
+  if (evt.keyCode == 39) {
+      rightDown = true;
+  }
+  else if (evt.keyCode == 37) {
+      leftDown = true;
+  } else if (evt.keyCode == 40) {
+      downDown = true;
+  } else if (evt.keyCode == 38) {
+      upDown = true;
+  } 
 }
 
 //and unset them when the right or left key is released
 function onKeyUp(evt) {
-  if (evt.keyCode == 39) rightDown = false;
-  else if (evt.keyCode == 37) leftDown = false;
+  if (evt.keyCode == 39) {
+      rightDown = false;
+  } else if (evt.keyCode == 37) {
+      leftDown = false;
+  } else if (evt.keyCode == 40) {
+      downDown = false;
+  } else if (evt.keyCode == 38) {
+      upDown = false;
+  }
 }
 
